fabf32f4887cfa817285f1979afe11c5e6b4b0c8
вαуℓσяαι <142450429+RetakeDevelopment@users.noreply.github.com>
2024-01-19 12:53:22 -0800

    added heading and pedanimation

--- config.lua
+++ config.lua
@@ -4,7 +4,7 @@ config = {}
 config.pedmodel = '' -- Pick the Ped Model Hash
 config.scenario = '' -- Pick the Ped Scenario to play, false to disable
 config.location = vec3(0.0, 50.0, 0.0)
-config.heading = (146.7)
+config.heading = 149.7562
 config.pedanimation = 
 
 ------ |  | ------
