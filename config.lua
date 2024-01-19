6f597d967d37cc69cb1c408f9f6820bad0fecd50
вαуℓσяαι <142450429+RetakeDevelopment@users.noreply.github.com>
2024-01-19 12:51:15 -0800

    Update config.lua

--- config.lua
+++ config.lua
@@ -5,5 +5,6 @@ config.pedmodel = '' -- Pick the Ped Model Hash
 config.scenario = '' -- Pick the Ped Scenario to play, false to disable
 config.location = vec3(0.0, 50.0, 0.0)
 config.heading = (146.7)
+config.pedanimation = 
 
 ------ |  | ------
