d2e4c2333d0e6c8f093c81487cccd3b885842aa5
вαуℓσяαι <142450429+RetakeDevelopment@users.noreply.github.com>
2024-01-19 12:50:56 -0800

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -8,4 +8,10 @@ function createDealerPed()
     Wait(10)
   end
 
-  dealerPed = CreatePed(5, dealerPedModel, config.location, config.heading
+  dealerPed = CreatePed(5, dealerPedModel, config.location, config.heading, false, false)
+  FreezeEntityPosition(dealerPed, true)
+  SetEntityInvincible(dealerPed, true)
+  SetBlockingOfNonTemporaryEvents(dealerPed, true)
+  SetModelAsNoLongerNeeded(dealerPedModel)
+  TaskStartScenarioInPlace(dealerPed, config.pedanimation, 0 ,true)
+  
