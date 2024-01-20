e0fc078e5a572e3061c31834809ec34eb1d517ea
вαуℓσяαι <142450429+RetakeDevelopment@users.noreply.github.com>
2024-01-20 14:51:11 -0800

    Ped Creation
    Creates PED, Checks if Player is within the RenderDistance set in Config, if player is in range, show ped, if player is not, remove the ped

--- client/client.lua
+++ client/client.lua
@@ -1,18 +1,62 @@
-function createDealerPed()
-  local dealerPedModel = dealer(config.pedmodel)
-  if not HasModelLoaded(dealerPedModel) then
-    RequestModel(dealerPedModel)
-    Wait(10)
-  end
-  while not HasModelLoaded(dealerPedModel) do
-    Wait(10)
+local function FetchModel(model)
+    RequestModel(GetHashKey(model))
+    while not HasModelLoaded(model) do 
+        Wait(100)
+    end
   end
 
-  dealerPed = CreatePed(5, dealerPedModel, config.location, config.heading, false, false)
-  FreezeEntityPosition(dealerPed, true)
-  SetEntityInvincible(dealerPed, true)
-  SetBlockingOfNonTemporaryEvents(dealerPed, true)
-  SetModelAsNoLongerNeeded(dealerPedModel)
-  TaskStartScenarioInPlace(dealerPed, config.pedanimation, 0 ,true)
+local LocalNPCs = {}
+
+local function GetLocalNPC(index)
+    return LocalNPCs[index]
 end
+
+local function CreateLocalNPC(index)
+    if (LocalNPCs[index]) then 
+        DestroyLocalNPC(index)
+    end
+      
+    LocalNPCs[index] = {}
+    local cfg = Config.WeedProccessBoss[index]
+  
+    FetchModel(cfg.WeedPedModel)
+    ------| CREATING PED |------
+    local weedProccessPed = CreatePed(1, cfg.WeedPedModel, cfg.WeedPedLocation, false, true)
+        FreezeEntityPosition(weedProccessPed, true)
+        SetEntityInvincible(weedProccessPed, true)
+        SetBlockingOfNonTemporaryEvents(weedProccessPed, true)
+    LocalNPCs[index].weedProccessPed = weedProccessPed
+end
+
+local function DestroyLocalNPC(index)
+    if (LocalNPCs[index]) then 
+        DeleteEntity(LocalNPCs[index].weedProccessPed)
+        LocalNPCs[index] = nill
+    end 
+end
+
+
+------| KEEP THIS AT BOTTOM |------
+------| CHECKS TO SEE IF PLAYER IS CERTAIN DISTANCE FROM THE PED |---
+Citizen.CreateThread(function()
+    while true do
+        local wait = 1000
+        local ped = PlayerPedId() 
+        local pcoords = GetEntityCoords(ped)
+        for i=1, 1 do 
+            local cfg = Config.WeedProccessBoss[i]
+            local coords = vector3(cfg.WeedPedLocation)
+            local dist = #(pcoords - coords)
+            local weedProccessPed = GetLocalNPC(i)
+            if dist < cfg.RenderDistance then 
+                if (GetLocalNPC(i) == nill) then 
+                  CreateLocalNPC(i)
+                end 
+            else 
+                DestroyLocalNPC(i)
+            end 
+        end
+      Wait(wait)
+    end
+end)
   
